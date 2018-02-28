59372c61995fea71772f55025926de585bc32479
toxAndreev <36535854+toxAndreev@users.noreply.github.com>
2018-02-28 14:01:35 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+var a,b,c;
+var arr = equation.split(' ');
+console.log(typeof(equation));
+a = Number(arr[0]);
+b = Number(arr[3]+arr[4]);
+c = Number(arr[7]+arr[8]);
+console.log('==================================')
+console.log(a)
+console.log(b)
+console.log(c)
+var x1,x2,D;
+var solutions;
+D = b*b -4*a*c;
+console.log(D)
+if(!D)
+solutions = '1 solution';
+if(D<0)
+solutions = null;
+if(D>0){
+x1 = (-b + Math.sqrt(D))/(2*a);
+x1 = Math.round(x1) ;
+x2 = (-b - Math.sqrt(D))/(2*a);
+x2 = Math.round(x2) ;
+if(x1<x2)
+solutions =[x1 ,x2];
+else
+solutions =[x2 ,x1];
+
 }
+return solutions;
+}
+
